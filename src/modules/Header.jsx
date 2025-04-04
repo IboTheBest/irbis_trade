8e9d38559bc01c1267356bfa27c3aabedac429ee
IboTheBest <cybergenyus@gmail.com>
2025-04-04 20:01:59 +0500

    new commit

--- src/modules/Header.jsx
+++ src/modules/Header.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import menu from '../assets/images/manu.svg';
-
+import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <header className="site-header pb-[100px] z-[2]">
             <div className="containers">
                 <nav className="top-0 bg-[#00000070] fixed w-full mx-auto left-0 right-0 flex items-center justify-between py-[10px]">
-                    <a className="ml-[30px]" href="./index.html">
+                    <Link className="ml-[30px]" to={"/"}>
                         <img className="untouchable" src="https://firebasestorage.googleapis.com/v0/b/cyber-beta-204b9.appspot.com/o/new_irbis_files%2Flogo.svg?alt=media&token=91595f4b-c369-4b3c-b460-07fb760d5570" alt="site logo" width="144" height="112" />
-                    </a>
+                    </Link>
                     <ul className="hidden md:flex md:mr-[30px] gap-[32px]">
                         {['Услуги', 'Продукты', 'О компании', 'Новости', 'Контакты', 'Партнерства'].map((text, index) => (
                             <li key={index}>
-                                <a className="font-bold md:text-[12px] lg:text-[18px] text-white duration-300 hover:text-[#00bfff] transition linear" href="#">{text}</a>
+                                <Link className="font-bold md:text-[12px] lg:text-[18px] text-white duration-300 hover:text-[#00bfff] transition linear" to={"/"}>{text}</Link>
                             </li>
                         ))}
                     </ul>
@@ -43,8 +43,8 @@ const Header = () => {
                         ))}
                     </ul>
                     <div className="flex items-center justify-center mt-[50px] gap-[20px]">
-                        <a className="bg-white border-none rounded-full w-[270px] h-[55px] font-bold transition-all duration-700 text-[#00bfff] hover:bg-[#00bfff] text-center hover:text-white flex items-center justify-center">Оформить груз</a>
-                        <a className="bg-white border-none rounded-full w-[270px] h-[55px] font-bold transition-all duration-700 text-[#00bfff] hover:bg-[#00bfff] text-center hover:text-white flex items-center justify-center">Получить консультацию</a>
+                        <Link className="bg-white border-none rounded-full w-[270px] h-[55px] font-bold transition-all duration-700 text-[#00bfff] hover:bg-[#00bfff] text-center hover:text-white flex items-center justify-center">Оформить груз</Link>
+                        <Link className="bg-white border-none rounded-full w-[270px] h-[55px] font-bold transition-all duration-700 text-[#00bfff] hover:bg-[#00bfff] text-center hover:text-white flex items-center justify-center">Получить консультацию</Link>
                     </div>
                 </div>
             </div>
