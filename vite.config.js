55ebd39abf8d518fb6dd9a85602f17fa6a8855eb
IboTheBest <cybergenyus@gmail.com>
2025-04-13 18:09:13 +0500

    new commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://irbis-trade.uz',
+        changeOrigin: true,
+        secure: false,
+        rewrite: path => path.replace(/^\/api/, '/api'),
+      }
+    }
+  }
+});
