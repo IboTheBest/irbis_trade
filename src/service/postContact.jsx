f78475d5ec16f49b59a165eb4e136814bb2ee218
IboTheBest <cybergenyus@gmail.com>
2025-04-13 18:16:11 +0500

    new commit

--- src/service/postContact.jsx
+++ src/service/postContact.jsx
@@ -1,9 +1,13 @@
 import { instance } from '../hooks/instance'
 
 const postContact = (api, body, toast) => {
-    instance().post(api, body).then(res => {
-        toast.success("Ваше сообщение успешно отправлено")
-    })
+    instance().post(api, body)
+        .then(() => {
+            toast.success("Ваше сообщение успешно отправлено")
+        })
+        .catch(error => {
+            toast.error("Ошибка при отправке сообщения")
+            console.error(error)
+        })
 }
-
 export default postContact
